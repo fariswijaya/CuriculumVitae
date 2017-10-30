1cbc66e52e9216ddc0b4f6508605f9cd5645b515
Faris Auzan <fariswijaya228@gmail.com>
2017-10-30 08:13:55 -0700

    Tugas Antar Muka Mobile

---
+++ app/src/main/java/com/example/farisauzan/curiculumvitae/DatadiriActivity.java
@@ -0,0 +1,30 @@
+package com.example.farisauzan.curiculumvitae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class DatadiriActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datadiri);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed(){super.onBackPressed();}
+}
